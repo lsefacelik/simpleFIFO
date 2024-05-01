7ad204fecd1dc55ec618c00ec95428e6b9a902f0
Sefa Celik <lsefacelik@yandex.com>
2024-05-01 21:40:34 +0300

    Fix format

--- fifo.h
+++ fifo.h
@@ -14,16 +14,16 @@
 extern "C" {
 #endif
 
-/*********** Includes ***********************************************************/
-#include<stdint.h>
-#include<stddef.h>
-#include<stdio.h>
+/*********** Includes */
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
 
-/*********** Exported constants *************************************************/
+/*********** Exported constants */
 #define FIFO_LEN 12000
 #define FIFO_DATA_TYPE uint8_t
 
-// Comment out to disable locking
+/* Comment out to disable locking */
 #define LOCK_MECHANISM_ENABLED_DEFINED
 #ifdef LOCK_MECHANISM_ENABLED_DEFINED
 #define LOCK_MECHANISM_ENABLED 1
@@ -35,60 +35,62 @@ extern "C" {
 #ifdef PRINT_ENABLED
 #define printd printf
 #else
-#define printd ((void *) 0)
+#define printd ((void *)0)
 #endif
 
-/*********** Exported macros ****************************************************/
+/*********** Exported macros */
 #if LOCK_MECHANISM_ENABLED
-#define FIFO_LOCK(HANDLE) do{						       	    \
-								if((HANDLE)->lock)				\
-									return FIFO_LOCKED;			\
-								else							\
-									(HANDLE)->lock = 1;			\
-							}while(0)
+#define FIFO_LOCK(HANDLE)                                                      \
+  do {                                                                         \
+    if ((HANDLE)->lock)                                                        \
+      return FIFO_LOCKED;                                                      \
+    else                                                                       \
+      (HANDLE)->lock = 1;                                                      \
+  } while (0)
 
 #define FIFO_UNLOCK(HANDLE) (HANDLE)->lock = 0;
 
-#define IS_FIFO_LOCKED(HANDLE) do{				                \
-								if((HANDLE)->lock)				\
-									return;						\
-								}while(0)
+#define IS_FIFO_LOCKED(HANDLE)                                                 \
+  do {                                                                         \
+    if ((HANDLE)->lock)                                                        \
+      return;                                                                  \
+  } while (0)
 #else
-#define FIFO_LOCK(HANDLE)		 ((void)0)
-#define FIFO_UNLOCK(HANDLE) 	 ((void)0)
-#define IS_FIFO_LOCKED(HANDLE)   ((void)0)
+#define FIFO_LOCK(HANDLE) ((void)0)
+#define FIFO_UNLOCK(HANDLE) ((void)0)
+#define IS_FIFO_LOCKED(HANDLE) ((void)0)
 #endif
 
-/*********** Exported types *****************************************************/
-typedef enum{
-	FIFO_ERROR = 0,
-	FIFO_EMPTY,
-	FIFO_FULL,
-	FIFO_WRITE_SUCCESSFUL,
-	FIFO_READ_SUCCESSFUL,
-	FIFO_NOT_ENOUGH_SPACE,
-	FIFO_NOT_ENOUGH_DATA,
+/*********** Exported types */
+typedef enum {
+  FIFO_ERROR = 0,
+  FIFO_EMPTY,
+  FIFO_FULL,
+  FIFO_WRITE_SUCCESSFUL,
+  FIFO_READ_SUCCESSFUL,
+  FIFO_NOT_ENOUGH_SPACE,
+  FIFO_NOT_ENOUGH_DATA,
 #if LOCK_MECHANISM_ENABLED
-	FIFO_LOCKED,
+  FIFO_LOCKED,
 #endif
-}fifo_ret_t;
+} fifo_ret_t;
 
 typedef FIFO_DATA_TYPE fifo_data_t;
 
-typedef struct{
-	size_t head;
-	size_t tail;
-	size_t len;
-	fifo_data_t buffer[FIFO_LEN];
-	uint8_t full;
+typedef struct {
+  size_t head;
+  size_t tail;
+  size_t len;
+  fifo_data_t buffer[FIFO_LEN];
+  uint8_t full;
 #if LOCK_MECHANISM_ENABLED
-	uint8_t lock;
+  uint8_t lock;
 #endif
-}fifo_t;
+} fifo_t;
 
-/*********** Exported variables *************************************************/
+/*********** Exported variables */
 
-/*********** Exported functions *************************************************/
+/*********** Exported functions */
 void fifo_init(fifo_t *);
 fifo_ret_t fifo_read_single(fifo_t *, fifo_data_t *);
 fifo_ret_t fifo_write_single(fifo_t *, fifo_data_t);
